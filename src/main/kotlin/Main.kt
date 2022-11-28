13b222e2c2f945055d15d3dd69005d952934d467
unknown <C:\Users\Pavel\AppData\Roaming\The Bat!>
2022-11-28 10:40:23 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+fun main() {
+
+    //Эта программа считает объем параллелепипеда
+    println("First side")
+    val side1 = readln().toInt()
+    println("Second side")
+    val side2 = readln().toInt()
+    println("Third side")
+    val side3 = readln().toInt()
+    val volume = side1*side2*side3
+
+    println("Volume papallelepiped")
+    println(volume)
+}
